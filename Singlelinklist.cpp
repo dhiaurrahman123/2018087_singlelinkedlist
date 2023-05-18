36d35193ab8374fbb18eaff9f036bb8bbaf8103e
dhiaurrahman123 <dhiaurrahman.i.ft18@mail.umy.ac.id>
2023-05-18 11:12:09 +0700

    mencari data

--- Singlelinklist.cpp
+++ Singlelinklist.cpp
@@ -89,6 +89,28 @@ void traverse() {
         }
     }
 }
+void searchData() {
+    if (listEmpty()) {
+        cout << "ListKosong" << endl;
+        system("pause");
+        system("cls");
+        return;
+    }
+    else {
+        int nim;
+        cout << "Masukkan NIM  : ";
+        cin >> nim;
+        Node* currentNode = START;
+        while (currentNode != NULL) {
+            if (currentNode->noMhs == nim) {
+                cout << "NIM:" << currentNode->noMhs << ",Nama: " <<currentNode->name << endl;
+                return;
+            }
+            currentNode = currentNode->next;
+        }
+        cout << "Data tidak ditemukan" << endl;
+    }
+}
 int main()
 {
     std::cout << "Hello World!\n";
